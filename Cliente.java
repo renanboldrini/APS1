ba7085d1762fdfa8c46b28f2e81744a2edd72983
renanboldrini <renanboldrini408@gmail.com>
2017-05-01 12:23:47 -0300

    Add files via upload

---
+++ Cliente.java
@@ -0,0 +1,13 @@
+
+public class Cliente {
+	Conta_Bancaria conta_bancaria;
+
+	public  Conta_Bancaria getConta_bancaria() {
+		return conta_bancaria;
+	}
+
+	public void setConta_bancaria(Conta_Bancaria conta_bancaria) {
+		this.conta_bancaria = conta_bancaria;
+	}
+	
+}
